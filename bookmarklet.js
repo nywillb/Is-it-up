9dd3c75289423eb8be5bf56defcb63a1b16612d7
nywillb <nywillb@users.noreply.github.com>
2015-12-14 10:49:06 -0500

    Quotes

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: window.open('http://downforeveryoneorjustme.com/' + location.href.replace('http://','').replace('https://',''), '_blank');
-if (location.href != http://nywillb.github.io/isItUp.) {
+if (location.href != "http://nywillb.github.io/isItUp") {
     javascript: window.open('http://downforeveryoneorjustme.com/' + location.href.replace('http://','').replace('https://',''), '_blank');
 } else {
     alert("Whoops, try dragging this button to your bookmark bar without clicking it!");
