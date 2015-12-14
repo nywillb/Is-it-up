81760641a2dc108a44f8de1e64ffb7b426b7d6b5
nywillb <nywillb@users.noreply.github.com>
2015-12-14 10:40:20 -0500

    Commit Current Codebase

---
+++ bookmarklet.js
@@ -0,0 +1,6 @@
+javascript: window.open('http://downforeveryoneorjustme.com/' + location.href.replace('http://','').replace('https://',''), '_blank'); if (hour < 18) { greeting = "Good day";}
+if (location.href != http://nywillb.github.io/isItUp.) {
+    javascript: window.open('http://downforeveryoneorjustme.com/' + location.href.replace('http://','').replace('https://',''), '_blank'); if (hour < 18) { greeting = "Good day";}
+} else {
+    alert("Whoops, try dragging this button to your bookmark bar without clicking it!");
+}
